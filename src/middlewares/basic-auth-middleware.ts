da08d9f8aa2fd95aca9ffca5f22615a13aece64d
prof-net <pavel.1c@mail.ru>
2022-10-19 13:21:41 +0300

    fix auth

--- src/middlewares/basic-auth-middleware.ts
+++ src/middlewares/basic-auth-middleware.ts
@@ -1,7 +1,7 @@
 import {NextFunction, Request, Response} from "express";
 
 export const basicAuthMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    if (req.headers.authorization?.split(" ")[1] !== "YWRtaW46cXdlcnR5") {
+    if (req.headers.authorization !== "Basic YWRtaW46cXdlcnR5") {
         res.send(401);
     } else {
         next();
