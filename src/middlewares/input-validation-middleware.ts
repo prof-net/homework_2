5dfae6eca9a2721980ee57de816350b2104dbf7a
prof-net <pavel.1c@mail.ru>
2022-10-19 13:25:14 +0300

    fix validation

--- src/middlewares/input-validation-middleware.ts
+++ src/middlewares/input-validation-middleware.ts
@@ -4,7 +4,7 @@ import {validationResult} from "express-validator";
 export const inputValidationMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        res.status(401).send({
+        res.status(400).send({
             errorsMessages: errors.array().map(err => {
                 return {
                     message: err.msg,
