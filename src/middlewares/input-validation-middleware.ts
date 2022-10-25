9a3cdd5d5d057077fe80ae1ab9596dfed085c49a
prof-net <pavel.1c@mail.ru>
2022-10-25 13:35:42 +0300

    fix validation

--- src/middlewares/input-validation-middleware.ts
+++ src/middlewares/input-validation-middleware.ts
@@ -5,11 +5,13 @@ export const inputValidationMiddleware = (req: Request, res: Response, next: Nex
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         res.status(400).send({
-            errorsMessages: errors.array().filter((v,i,a)=>a.findIndex(t=>(t.param === v.param))===i).map(err => {
-                return {
-                    message: err.msg,
-                    field: err.param
-                }
+            errorsMessages: errors.array({onlyFirstError: true})
+                // .filter((v,i,a)=>a.findIndex(t=>(t.param === v.param))===i)
+                .map(err => {
+                    return {
+                        message: err.msg,
+                        field: err.param
+                    }
             })
         })
     } else {
