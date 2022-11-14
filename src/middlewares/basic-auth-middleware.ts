2407a57826c7ac2406c396eff2dc524f827489f8
prof-net <pavel.1c@mail.ru>
2022-11-14 19:24:06 +0300

    auth test

--- src/middlewares/basic-auth-middleware.ts
+++ src/middlewares/basic-auth-middleware.ts
@@ -2,28 +2,31 @@ import {NextFunction, Request, Response} from "express";
 import {usersQueryRepository} from "../repositories/users-query-repository";
 
 export const basicAuthMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+    if (req.headers.authorization === "Basic YWRtaW46cXdlcnR5") {
+        next();
+        return;
+    }
+    const authString = req.headers.authorization;
 
-    // const authString = req.headers.authorization;
-    //
-    // if (!authString) {
-    //     res.sendStatus(401);
-    // } else {
-    //
-    //     const authArr = authString.split(' ');
-    //     const authLoginPassArr = Buffer.from(authArr[1], 'base64').toString('utf-8').split(':');
-    //     const user = await usersQueryRepository.getOneUserForLogin(authLoginPassArr[0])
-    //
-    //     if (!user) {
-    //         res.sendStatus(401);
-    //     } else {
-    //         if (authArr[0] === "Basic" && Buffer.from(user.password, 'base64').toString('utf-8') === authLoginPassArr[0]) {
-    //             next();
-    //         } else {
-    //             res.sendStatus(401);
-    //         }
-    //     }
-    //
-    // }
-    next();
+    if (!authString) {
+        res.sendStatus(401);
+    } else {
+
+        const authArr = authString.split(' ');
+        const authLoginPassArr = Buffer.from(authArr[1], 'base64').toString('utf-8').split(':');
+        const user = await usersQueryRepository.getOneUserForLogin(authLoginPassArr[0])
+
+        if (!user) {
+            res.sendStatus(401);
+        } else {
+            if (authArr[0] === "Basic" && Buffer.from(user.password, 'base64').toString('utf-8') === authLoginPassArr[0]) {
+                next();
+            } else {
+                res.sendStatus(401);
+            }
+        }
+
+    }
+
 
 }
