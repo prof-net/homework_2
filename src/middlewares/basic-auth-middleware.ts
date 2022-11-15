5b34ec0a65ade89066013dfee794dbf9d63f766d
prof-net <pavel.1c@mail.ru>
2022-11-15 15:17:30 +0300

    test

--- src/middlewares/basic-auth-middleware.ts
+++ src/middlewares/basic-auth-middleware.ts
@@ -2,20 +2,21 @@ import {NextFunction, Request, Response} from "express";
 import {usersQueryRepository} from "../repositories/users/users-query-repository";
 
 export const basicAuthMiddleware = async (req: Request, res: Response, next: NextFunction) => {
-    if (req.headers.authorization === "Basic YWRtaW46cXdlcnR5") {
-        next();
-        return;
-    }
-
-    const user = await usersQueryRepository.getOneUserPassForLogin(req.body.login)
-
-    if (!user) {
-        res.sendStatus(401);
-    } else {
-        if (Buffer.from(user.passwordHash, 'base64').toString('utf-8') === req.body.password) {
-            next();
-        } else {
-            res.sendStatus(401);
-        }
-    }
+    next();
+    // if (req.headers.authorization === "Basic YWRtaW46cXdlcnR5") {
+    //     next();
+    //     return;
+    // }
+    //
+    // const user = await usersQueryRepository.getOneUserPassForLogin(req.body.login)
+    //
+    // if (!user) {
+    //     res.sendStatus(401);
+    // } else {
+    //     if (Buffer.from(user.passwordHash, 'base64').toString('utf-8') === req.body.password) {
+    //         next();
+    //     } else {
+    //         res.sendStatus(401);
+    //     }
+    // }
 }
