f84cca0cc17d28f822815107137298ded0318af0
prof-net <pavel.1c@mail.ru>
2022-11-15 17:30:07 +0300

    fix put delete

--- src/routes/comments-router.ts
+++ src/routes/comments-router.ts
@@ -32,7 +32,7 @@ commentsRouter.put('/comments/:id',
     async (req: RequestWithParamsBody<{ id: string }, ICommentBody>, res: Response) => {
 
         const comment = await commentsQueryRepository.getSingleComment(req.params.id);
-        if (req.user!.id !== comment?.userId) {
+        if (comment && req.user!.id !== comment?.userId) {
             res.sendStatus(403);
             return;
         }
